6bbaec38b50b3d2131a92300b19d6fc7bef8c7d3
coder0112 <zafarbekegamov0112@gmail.com>
2022-11-11 11:48:00 +0500

    pass all tests

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int max = 0;
+        if (first > second) {
+            max = first;
+        } else {
+            max = second;
+        }
+        System.out.println(max);
     }
 }
